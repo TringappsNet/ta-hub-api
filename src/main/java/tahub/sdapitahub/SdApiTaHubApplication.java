a2cdbce6eb612ef6ee859862e499f302365d192c
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-04-30 15:00:35 +0530

    SpringBoot backend created

---
+++ src/main/java/tahub/sdapitahub/SdApiTaHubApplication.java
@@ -0,0 +1,13 @@
+package tahub.sdapitahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdApiTaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdApiTaHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tahub/sdapitahub/SdApiTaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package tahub.sdapitahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdApiTaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
