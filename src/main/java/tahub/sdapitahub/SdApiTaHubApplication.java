1b039b5612e1ea8361feae54132f4d7c22a80511
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-05-02 15:46:00 +0530

    CORS Resolved

--- src/main/java/tahub/sdapitahub/SdApiTaHubApplication.java
+++ src/main/java/tahub/sdapitahub/SdApiTaHubApplication.java
@@ -3,10 +3,14 @@ package tahub.sdapitahub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
+@ComponentScan(basePackages = "tahub.sdapitahub.Config")
 public class SdApiTaHubApplication {
 
 	public static void main(String[] args) {
