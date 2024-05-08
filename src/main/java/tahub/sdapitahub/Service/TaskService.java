6f771df62db5d7b6e2c1d773202cc5ef43d67457
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-05-08 10:50:25 +0530

    Creating tasks from JobRequirement Property (positions)

--- src/main/java/tahub/sdapitahub/Service/TaskService.java
+++ src/main/java/tahub/sdapitahub/Service/TaskService.java
@@ -1,4 +1,64 @@
 package tahub.sdapitahub.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tahub.sdapitahub.Entity.Task;
+import tahub.sdapitahub.Repository.TaskRepository;
+import tahub.sdapitahub.DTO.TaskDTO;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> createTasks(List<TaskDTO> taskDTOs) {
+        List<Task> tasks = new ArrayList<>();
+        for (TaskDTO taskDTO : taskDTOs) {
+            Task task = new Task();
+            task.setJobTitle(taskDTO.getJobTitle());
+            task.setRoleType(taskDTO.getRoleType());
+            task.setModeOfWork(taskDTO.getModeOfWork());
+            task.setWorkLocation(taskDTO.getWorkLocation());
+            tasks.add(taskRepository.save(task));
+        }
+        return tasks;
+    }
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(TaskDTO taskDTO) {
+        Task task = new Task();
+        task.setJobTitle(taskDTO.getJobTitle());
+        task.setRoleType(taskDTO.getRoleType());
+        task.setModeOfWork(taskDTO.getModeOfWork());
+        task.setWorkLocation(taskDTO.getWorkLocation());
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, TaskDTO taskDTO) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            task.setJobTitle(taskDTO.getJobTitle());
+            task.setRoleType(taskDTO.getRoleType());
+            task.setModeOfWork(taskDTO.getModeOfWork());
+            task.setWorkLocation(taskDTO.getWorkLocation());
+            return taskRepository.save(task);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
 }
