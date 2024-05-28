2db44d9d599799db198bb9876f914ecbaa1ae3f1
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-05-28 12:11:41 +0530

    job approval API fixed

--- src/main/java/tahub/sdapitahub/repository/query/JobRequirementQuery.java
+++ src/main/java/tahub/sdapitahub/repository/query/JobRequirementQuery.java
@@ -4,7 +4,7 @@ public enum JobRequirementQuery {
     FIND_ALL("SELECT * FROM ta_client_requirements"),
     FIND_BY_ID("SELECT * FROM ta_client_requirements WHERE job_id = ?"),
     FIND_BY_APPROVAL_TOKEN("SELECT * FROM ta_client_requirements WHERE approval_token = ?"),
-    FIND_BY_APPROVED_BY("SELECT * FROM ta_client_requirements WHERE approved_by = ? AND approval_status = false"),
+    FIND_BY_APPROVED_BY("SELECT * FROM ta_client_requirements WHERE approved_by = ?"),
     SAVE("INSERT INTO ta_client_requirements (requirement_start_date, client_name, client_spoc_name, client_spoc_contact, account_manager, account_manager_email, total_no_of_openings, salary_budget, mode_of_interviews, tentative_start_date, tentative_duration, approved_by, approval_status, approval_token, created_at, last_updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"),
     FIND_BY_CLIENT_NAME("SELECT * FROM ta_client_requirements WHERE client_name = ?"),
     DELETE_BY_ID("DELETE FROM ta_client_requirements WHERE job_id = ?");
